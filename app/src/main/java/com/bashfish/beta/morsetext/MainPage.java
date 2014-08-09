3bb380f8700ba6c5162118954153d53924e0c7cd
Ryan Guthridge <ryanguthridge@gmail.com>
2014-08-09 02:08:15 -0400

    Morse Code text string is now being displayed at the top of the phone and the ability to type a single word with dots and dashes now works by using clicks and long presses

--- app/src/main/java/com/bashfish/beta/morsetext/MainPage.java
+++ app/src/main/java/com/bashfish/beta/morsetext/MainPage.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainPage extends Activity {
@@ -102,6 +103,10 @@ public class MainPage extends Activity {
         morseString += lastMorseCode;
 
         Log.d("Morse Code", morseString);
+
+        TextView morseCodeTextDisplay = (TextView)findViewById(R.id.morseTextDisplay);
+        morseCodeTextDisplay.append(lastMorseCode);
+
 
         return morseString;
     }
